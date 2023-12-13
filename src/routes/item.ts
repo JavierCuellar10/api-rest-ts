1a4f9ec0fc19f8ca3142917807bfdd50d798eeb7
JavierCuellar10 <javiercuellarbermeo@gmail.com>
2023-12-13 17:28:54 -0500

    Primer commit

---
+++ src/routes/item.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { Request, Response } from 'express';
+
+const router = Router()
+
+/**
+ * http://localhost:3002/items [GET]
+ */
+router.get('/items', (req: Request, res: Response) => {
+    res.send({data: "AQUI_VAN_LOS_MODELOS"});
+});
+
+
+
+
+
+export{router};
